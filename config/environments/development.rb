e2dc71c0798e585b5ee2599c00137274775e9052
James Mwangi K <jameshmwangiy@gmail.com>
2026-02-12 12:31:50 +0300

    dev async jobs fallback

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Use async adapter in development to avoid needing Redis/Sidekiq locally
+  config.active_job.queue_adapter = :async
 
   config.action_mailer.perform_caching = false
 
